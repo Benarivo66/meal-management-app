7b4aeb314d96f96c235db98dfe3fba980a255ca8
benarivo66 <iz4comed@yahoo.com>
2023-01-09 14:39:39 +0100

    add env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import dbSetup from './addonUserCategory/db/setup';
+import { config } from 'dotenv'; 
+
+config();
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   dbSetup();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
