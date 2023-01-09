771a776af92aeb2acca79be332f8ba62d78791b1
benarivo66 <iz4comed@yahoo.com>
2023-01-09 21:24:45 +0100

    some changes

--- src/addonUserCategory/db/knexfile.ts
+++ src/addonUserCategory/db/knexfile.ts
@@ -6,10 +6,11 @@
 import { config } from 'dotenv';
 
 config();
+console.log(111, process.env.USER)
 
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: process.env.CLIENT || 'postgresql',
     connection: {
       database: process.env.DATABASE,
       user: process.env.USER,
@@ -21,7 +22,26 @@ module.exports = {
     },
     migrations: {
       directory:
-        '/Users/Mac/Documents/meal-app/src/addonUserCategory/db/migrations',
+      __dirname + '/db/migrations',
+      tableName: 'knex_migrations',
+      extension: 'ts',
+    },
+  },
+
+  production: {
+    client: process.env.CLIENT || 'postgresql',
+    connection: {
+      database: process.env.DATABASE,
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory:
+      __dirname + '/db/migrations',
       tableName: 'knex_migrations',
       extension: 'ts',
     },
